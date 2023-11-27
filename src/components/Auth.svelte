ac562cf97c31117cb48b9c9e2e9e75d58740d52c
rancharaz <ranveer1990@hotmail.com>
2023-11-28 00:52:20 +0400

    initial commit

---
+++ src/components/Auth.svelte
@@ -0,0 +1,76 @@
+<div class="authContainer">
+    <form>
+        <h1>Login</h1>
+        <label for="">
+            <input type="email" placeholder="Email">
+        </label>
+        <label for="">
+            <input type="password" placeholder="Password">
+        </label>
+        <label for="">
+            <input type="password" placeholder="Confirm Password">
+        </label>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
+<style>
+    .authContainer{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        flex: 1;
+    }
+    form{
+        display: flex;
+        flex-direction: column;
+        gap: 14px;
+        width: 400px;
+        max-width: 100%;
+        margin:  0 auto;
+    }
+    form input {
+        width: 100%;
+        border: none;
+        background: transparent;
+        color: white;
+    }
+    h1 {
+        text-align: center;
+        font-size: 3rem;
+        
+    }
+    form input {
+ 
+        border: none;
+        background: transparent;
+        color: white;
+        padding:  14px;
+        font-size: 14px;
+    }
+    form input:focus {
+       border:none;
+       outline: none;
+       }
+       form label {
+        position: relative;
+        border: 1px solid blue;
+        border-radius: 5px;
+       }
+       form button {
+        background-color: navy;
+        color: white;
+        outline: none;
+        border: none;
+        padding: 10px;
+        text-transform: uppercase;
+        border-radius: 5px;
+        cursor: pointer;
+        transition: ease-in-out;
+        font-size: 1.2rem;
+       }
+       form button:hover {
+        transition: ease-in-out;
+        background-color: blue;
+       }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+
+<div class="mainContainer">
+    <slot />
+    <style>
+        .mainContainer {
+            min-height: 100vh;
+            background: linear-gradient(to right, #000428, #000046 );
+            color: white;
+            position: relative;
+            display: flex;
+            flex-direction: column;
+            padding: 2rem;
+
+        }
+    </style>
+</div>
